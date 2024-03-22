e4e7728ddd8e4252e8a325424185709753ce7101
Senibo-Don-Pedro <senibodonpedro@gmail.com>
2024-03-22 14:08:14 +0100

    added feature to addhotel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,19 @@
-import express from 'express'
+import express, {Request, Response  } from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import mongoose from 'mongoose'
 import userRoutes from './routes/users'
 import authRoutes from './routes/auth'
+import myHotelRoutes from './routes/my-hotels'
 import cookieParser from 'cookie-parser'
 import path from 'path'
+import { v2 as cloudinary} from "cloudinary"
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 //connect to db
 mongoose.connect(process.env.MONGO_DB as string)
@@ -27,6 +35,11 @@ app.use(express.static(path.join(__dirname, "../../front-end/dist")))
 //user routes
 app.use('/api/auth', authRoutes )
 app.use('/api/users', userRoutes )
+app.use('/api/my-hotels', myHotelRoutes)
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../front-end/dist/index.html"))
+})
 
 //listening
 app.listen(7000, () => {
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,8 +2,12 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import { Layout } from "./layouts/Layout";
 import { Register } from "./pages/Register";
 import { SignIn } from "./pages/SignIn";
+import { AddHotel } from "./pages/AddHotel";
+import { useAppContext } from "./context/AppContext";
+
 
 const App = () => {
+  const { isLoggedIn } = useAppContext()
   return (
     <Router>
       <Routes>
@@ -27,7 +31,14 @@ const App = () => {
             <SignIn/>
           </Layout>
         }/>
-        <Route path="*" element={<Navigate to={"/"} />} />
+        {isLoggedIn && <>
+          <Route path= "/add-hotel" element= {
+            <Layout>
+              <AddHotel/>
+            </Layout>
+          }/>
+        </>}
+        <Route path="*" element={<Navigate to={"/"} />}/>
       </Routes>
     </Router>
   )
